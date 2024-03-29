620ae5b8e50cd4c89c60f706fa907e5c7e305611
nzkks <kkshanthosh@gmail.com>
2024-03-29 18:40:42 +1300

    Replace connect and mapStateToProps and mapDispatchToProps with hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
-import CakeContainer from './components/CakeContainer';
 import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
