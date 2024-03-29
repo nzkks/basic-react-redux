2cfd8443cb5058b306f2bea4f2c0794a395eed77
nzkks <kkshanthosh@gmail.com>
2024-03-29 16:35:03 +1300

    Add Cake container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import CakeContainer from './components/CakeContainer';
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
 }
 
 export default App;
