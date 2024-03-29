299d6c904a8d7f1a29aeaaeb18c219535b7282f5
nzkks <kkshanthosh@gmail.com>
2024-03-29 22:27:22 +1300

    mapStateToProps has second parameter 'ownProps' which is the component's one. Usage example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './redux/store';
+import ItemContainer from './components/ItemContainer';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
@@ -11,6 +12,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <HooksIceCreamContainer />
