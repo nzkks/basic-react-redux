4cd54bd7436afc616fccaedc62a13c3259b01f81
nzkks <kkshanthosh@gmail.com>
2024-03-29 20:14:30 +1300

    Add Ice Creams to the store and do the same actions as cakes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './redux/store';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import HooksIceCreamContainer from './components/HooksIceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <HooksCakeContainer />
+        <HooksIceCreamContainer />
       </div>
     </Provider>
   );
